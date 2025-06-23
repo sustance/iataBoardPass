1077198eca0da351182ea65d83e0af9415cbb7af
Kym Michael <kym.michael@gmail.com>
2025-06-23 09:25:18 +0800

    Create Fetch_Git_Run_Travel_Analysis.sh

---
+++ Fetch_Git_Run_Travel_Analysis.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+ # Download the scripts
+gith="https://raw.githubusercontent.com/sustance/iataBoardPass/refs/heads/main/"
+
+# Start ssh-agent if not running
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    eval "$(ssh-agent -s)"
+fi
+
+# Add your key to the agent (will prompt for passphrase once)
+ssh-add ~/.ssh/id_ed25519  # or whatever your private key is named
+
+# THIS FAILS UNLESS THE DIRECTORY IS EMPTY
+rm $HOME/tmp/*
+git clone git@github.com:sustance/pdata.git $HOME/tmp
+# passphrase is "kym.michael@gmail.com" perhaps need to do manually once per session
+ 
+/bin/curl https://raw.githubusercontent.com/sustance/iataBoardPass/refs/heads/main/scansRawToCompact.py -o $HOME/tmp/scansRawToCompact.py
+#/bin/curl ${gith}scansRawToCompact.py -o $HOME/tmp/scansRawToCompact.py
+ 
+ 
+ 
+ # Check if YEAR is set
+ if [ -z "$YEAR" ]; then
+     echo "Error: YEAR environment variable not set."
+     echo "Usage: $YEAR bpScanList.sh"
+     exit
+ fi
+ 
+ YEAR_FILE="${YEAR}raw.csv"
+ echo "Working on Year $YEAR ..."
+
+YEAR=${YEAR} $HOME/tmp/scansRawToCompact.py
